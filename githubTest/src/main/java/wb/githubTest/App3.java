6b9a66fe4aa083b44f3c85da6fb30f0896a2108f
wangbo <526477565@qq.com>
2017-10-30 10:28:29 +0800

    测试分支1

---
+++ githubTest/src/main/java/wb/githubTest/App3.java
@@ -0,0 +1,13 @@
+package wb.githubTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App3 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
